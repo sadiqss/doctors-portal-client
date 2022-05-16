c874548e7352744559a7ed4fa3cddf0fd9421522
sadiqss <sadiq.sau48@gmail.com>
2022-05-17 00:04:37 +0100

    error handled

--- src/Pages/Appoinment/Service.js
+++ src/Pages/Appoinment/Service.js
@@ -13,7 +13,7 @@ const Service = ({ service, setTreatment }) => {
                 }</p>
                 <p>{slots.length} {slots.length > 1 ? 'spaces' : 'space'} Available</p>
                 <div className="card-actions justify-center">
-                    <label for="booking-modal"
+                    <label htmlFor="booking-modal"
                         disabled={slots.length === 0}
                         onClick={() => setTreatment(service)}
                         className="btn btn-sm bg-gradient-to-r from-secondary btn-secondary text-white uppercase">Book Appointment</label>
