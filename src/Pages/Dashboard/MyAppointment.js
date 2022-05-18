8a49235b504ec340893a32bca7f647830d08d26c
sadiqss <sadiq.sau48@gmail.com>
2022-05-18 02:05:19 +0100

    dashboard with nested route outlet drawer

---
+++ src/Pages/Dashboard/MyAppointment.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MyAppointment = () => {
+    return (
+        <div>
+            <h2>My appointment</h2>
+        </div>
+    );
+};
+
+export default MyAppointment;
