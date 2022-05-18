1779645fd1cb4b4eea36c6ea5998af98c0d82941
sadiqss <sadiq.sau48@gmail.com>
2022-05-18 01:46:44 +0100

    react query for available data reload

--- src/Pages/Appoinment/BookingModal.js
+++ src/Pages/Appoinment/BookingModal.js
@@ -4,7 +4,7 @@ import { useAuthState } from 'react-firebase-hooks/auth';
 import auth from '../../firebase.init';
 import { toast } from 'react-toastify';
 
-const BookingModal = ({ date, treatment, setTreatment }) => {
+const BookingModal = ({ date, treatment, setTreatment, refetch }) => {
     const { _id, name, slots } = treatment;
     const [user, loading, error] = useAuthState(auth);
     const handleBooking = event => {
@@ -38,6 +38,7 @@ const BookingModal = ({ date, treatment, setTreatment }) => {
                 else {
                     toast.error(`You already have an appointment on ${data.booking?.date} at ${data.booking?.slot}`)
                 }
+                refetch();
                 setTreatment(null);
             });
 
